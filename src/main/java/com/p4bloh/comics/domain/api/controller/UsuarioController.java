87f0a623a537e4c3d3e650069fe9afcea2ac94fa
pabloxcastro <pablo.castro@hotmail.com>
2021-07-24 11:14:19 -0300

    feat listar e alterar usuario

--- src/main/java/com/p4bloh/comics/domain/api/controller/UsuarioController.java
+++ src/main/java/com/p4bloh/comics/domain/api/controller/UsuarioController.java
@@ -1,18 +1,23 @@
 package com.p4bloh.comics.domain.api.controller;
 
+import com.p4bloh.comics.domain.dto.UsuarioPutRequest;
+import com.p4bloh.comics.domain.dto.UsuarioPutResponse;
 import com.p4bloh.comics.domain.dto.UsuarioRequest;
 import com.p4bloh.comics.domain.dto.UsuarioResponse;
+import com.p4bloh.comics.domain.exception.EntidadeNaoEncontradaException;
 import com.p4bloh.comics.domain.model.Usuario;
+import com.p4bloh.comics.domain.repository.UsuarioRepository;
 import com.p4bloh.comics.domain.service.UsuarioService;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "api/usuarios")
@@ -21,10 +26,55 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @PostMapping
     @ApiOperation(value = "Cadastro de usuários")
     public ResponseEntity<?> adicionar(@Valid @RequestBody UsuarioRequest usuarioRequest){
         Usuario usuarioNovo = usuarioService.salvar(usuarioRequest.toObj());
         return ResponseEntity.status(201).body(UsuarioResponse.toDto(usuarioNovo));
     }
+
+    @GetMapping
+    @ApiOperation(value = "Lista de usuários")
+    public ResponseEntity<?> listar(){
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        if (usuarios == null) return ResponseEntity.notFound().build();
+
+        List<UsuarioResponse> usuarioResponses = new ArrayList<>();
+
+        for (Usuario usuario: usuarios){
+            usuarioResponses.add(UsuarioResponse.toDto(usuario));
+        }
+
+        return ResponseEntity.ok(usuarioResponses);
+
+    }
+    
+    @PatchMapping("/{id}")
+    @ApiOperation(value = "Atualização de nome e data de nascimento")
+    public ResponseEntity<?> atualizarUsuario(@PathVariable Long id,
+                                              @RequestBody UsuarioPutRequest usuarioRequest){
+        try {
+            Optional<Usuario> usuarioAtual = usuarioRepository.findById(id);
+
+            if (usuarioAtual.isEmpty()){
+                throw new EntidadeNaoEncontradaException("Usuário", id);
+            }
+
+            BeanUtils.copyProperties(usuarioRequest.toObj(), usuarioAtual.get(),
+                    "id", "cpf", "email");
+
+            Usuario usuarioAtualizado = usuarioRepository.save(usuarioAtual.get());
+
+            return ResponseEntity.ok(UsuarioPutResponse.toDto(usuarioAtualizado));
+
+        } catch (EntidadeNaoEncontradaException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
 }
